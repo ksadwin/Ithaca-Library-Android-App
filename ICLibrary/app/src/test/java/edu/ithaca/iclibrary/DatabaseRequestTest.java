a4fc427548de2e0cb6af8b0ed605e9454326eaf2
ksadwin <klizzinator@gmail.com>
2016-03-29 21:28:23 -0400

    I'm just not cut out for this testing stuff.

--- ICLibrary/app/src/test/java/edu/ithaca/iclibrary/DatabaseRequestTest.java
+++ ICLibrary/app/src/test/java/edu/ithaca/iclibrary/DatabaseRequestTest.java
@@ -1,17 +1,28 @@
 package edu.ithaca.iclibrary;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.net.URL;
 
 /**
  * Created by Kelly on 3/29/2016.
  */
+//@RunWith(RoboelectricCustomTestRunner.class)
 public class DatabaseRequestTest {
+
+    @Mock DatabaseRequest req;
+
+    @Before
+    public void setup () {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void testingTests() {
-        DatabaseRequest req = new DatabaseRequest();
         req.execute(new URL[0]);
         System.out.println("does this run");
     }
