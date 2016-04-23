e3fdff3410862c33c9ec542602fe23f276d59044
Joseph Yull <jyull1@ithaca.edu>
2016-04-23 15:52:13 -0400

    UI Tests Setup

--- ICLibrary/app/src/androidTest/java/edu/ithaca/iclibrary/ApplicationTest.java
+++ ICLibrary/app/src/androidTest/java/edu/ithaca/iclibrary/ApplicationTest.java
@@ -1,13 +1,37 @@
 package edu.ithaca.iclibrary;
 
 import android.app.Application;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.test.ApplicationTestCase;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+@RunWith(AndroidJUnit4.class)
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void changeText_sameActivity(){
+        Espresso.onView(ViewMatchers.withId(R.id.editQuery)).
+                perform(ViewActions.click(), ViewActions.typeText("dickens"));
+
+        Espresso.closeSoftKeyboard();
     }
 }
--- ICLibrary/app/src/test/java/edu/ithaca/iclibrary/ExampleUnitTest.java
+++ ICLibrary/app/src/test/java/edu/ithaca/iclibrary/ExampleUnitTest.java
@@ -20,36 +20,9 @@ import static org.junit.Assert.assertNotNull;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
-    @Mock
-    MainActivity sampleMain = Mockito.mock(MainActivity.class);
 
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    public void mainActivityBuildsSuccessfully() throws Exception {
-        assertNotNull(sampleMain);
-    }
-
-    @Test
-    public void spinnerLoadedAtOnCreate() throws Exception {
-        assertSame((Spinner) sampleMain.findViewById(R.id.searchType), sampleMain.spinnerLoader(R.id.searchType));
-    }
-
-    @Test
-    public void spinnerLoaderCreatesSpinner() throws Exception {
-        Spinner spin = sampleMain.spinnerLoader(R.id.searchType);
-        assertNotNull(spin);
-    }
-
-    @Test
-    public void spinnerHasCorrectSearchTypesEnabled() throws Exception {
-
-//            assertEquals("Author", sampleMain.spinnerLoader(R.id.searchType).getItemAtPosition(0));
-//            assertEquals("ISBN",sampleMain.spinnerLoader(R.id.searchType).getItemAtPosition(1));
-//            assertEquals("Subject",sampleMain.spinnerLoader(R.id.searchType).getItemAtPosition(2));
-//            assertEquals("Title",sampleMain.spinnerLoader(R.id.searchType).getItemAtPosition(3));
     }
 }
