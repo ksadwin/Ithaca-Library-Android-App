60b50ff93c0cbd9798394affee5d9a7d2b7e790f
Joseph Yull <jyull1@ithaca.edu>
2016-05-05 21:03:18 -0400

    Ohmygod

--- ICLibrary/app/src/main/java/edu/ithaca/iclibrary/ScrollingActivity.java
+++ ICLibrary/app/src/main/java/edu/ithaca/iclibrary/ScrollingActivity.java
@@ -183,7 +183,7 @@ public class ScrollingActivity extends AppCompatActivity {
     }
 
     private void makeDetailActivity(Material book) {
-        Intent i = new Intent(this, ResultActivity.class);
+        Intent i = new Intent(this, DetailActivity.class);
         i.putExtra("bibtext1", book.getBibText1());
         i.putExtra("bibtext2", book.getBibText2());
         i.putExtra("status", book.translateItemStatusCode());
